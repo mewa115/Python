c774cbb0e0fadc938ded1046fdf12ab339e0eb65
mewa115 <37215115+mewa115@users.noreply.github.com>
2018-03-17 12:29:57 +0200

    Add files via upload

--- Data Camp 1.py
+++ Data Camp 1.py
@@ -75,8 +75,80 @@ print(fam5)
 # Specific methods are applied to different types,
 
 fam = fam + ['nik', 1.80]
+print(fam)
 print(fam.index('ler'))  # fam is an object with the type of list. Index is a method. As a result I will receive an element with index 4
 print(fam.count('nik'))  # count how many times I have nik in the list fam
 
 mom = 'svet'
-print(mom.replace('svet', 'mom'))
+#mom = mom.capitalize()
+mom = mom.replace('svet', 'mom')  # method replace replaces svet with mom
+mom = mom.capitalize()  # method capitalized makes the first letter capitalized
+print(mom)
+
+print(fam)
+fam.append('nata')
+fam.append(1.65)  #  it adds new value to the list bit it did not.
+print(fam)
+fam.reverse()  # makes a reverse order for the elements in the list
+print(fam)
+
+help(fam.append)  # help function
+
+mom = 'svet'
+mom = mom.upper()  # this method makes all characters with capital size.
+print(mom)
+
+
+help(fam.append)
+
+
+#  Packages is a directory of python scripts
+#  pkg/mod.py
+#  packages should be installed.
+#  additional packages are PIPs
+
+
+import romeo  # import py file
+print(romeo)  # show the path?
+
+# I have downloaded  PIP file from http://pip.readthedocs.io/en/stable/installing/
+# after that I executed downloaded .py file with python interpratator
+# that gave me an opportunity to import different packages
+
+import numpy
+import math  # here is a full import of math package
+r = 0.43
+C = 2*r*math.pi
+A = math.pi*r**2
+C = round(C, 2)
+A = round(A, 2)
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+# Definition of radius
+r = 192500
+# Import radians function of math package
+from math import radians
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = r * radians(12)
+# Print out dist
+print(dist)
+
+#  There are several ways to import packages and modules into Python. Depending on the import call, you'll have to use different Python code.
+#  suppose you want to use the function inv(), which is in the linalg subpackage of the scipy package. You want to be able to use this function as follows:
+# I want to import function and name it as my_inv
+#  from scipy.linalg import inv as my_inv
+
+print(dist) # test
+
+import numpy as np
+
+
+np_height = np.array(height)
+np_height
+
+np_weight = np.array(weight)
+np_weight
+
+
+
